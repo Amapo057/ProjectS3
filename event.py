bacabd9e28cd866d0b759c15cd8d98631c1807a8
Amapo057 <jiseong2002@naver.com>
2024-11-24 22:21:45 +0900

    Co-authored-by: Shar0n-4885 <Shar0n-4885@users.noreply.github.com>

--- event.py
+++ event.py
@@ -1,18 +1,16 @@
 import random as rd
-# q1 = 무전기, q2 = 보존식량, q3 = 총 모두 1로 만드는 조건 있어야함
+# q1 = 라디오, q2 = 보존식량, q3 = 총 모두 1로 만드는 조건 있어야함
 
 # 상태 설정
 hp = 3
 san = 3
-die = 0
 
 # 엔딩 조건
 q1 = 0
 q2 = 0
 q3 = 0
 
-
-def day1(r):
+def event1(r):
     global hp, san
     global q1, q2, q3
     # 랜덤 1일때
@@ -46,7 +44,7 @@ def day1(r):
                 if ans2 == 1:
                     if rd.randint(1, 10) > 6:
                         print("도움이 될거에요")
-                        print("무전기를 얻었다")
+                        print("라디오를 얻었다")
                         q1 = 1
                     else:
                         print("더러우니까 말걸지 마세요.")
@@ -92,7 +90,7 @@ def day1(r):
     # 랜덤 3일때
     else:
         print('피곤함이 몰려온다')
-        ans = int(input('1: 졸음을 참는다 2: 잔다'))
+        ans = int(input('1: 졸음을 참는다 2: 잔다   '))
         if ans == 1:
             if rd.randint(1, 10) <= 5:
                 print('정신이 몽롱하다. 정신력 하락 1')
@@ -110,6 +108,84 @@ def day1(r):
             else:
                 print('눈을 뜨니 내 방이다. 그렇다 꿈이였다.. 히든엔딩')
                 exit(0)
-def day2(r, point):
-    if point == 1:
-        print(" ")
+def event2(r):
+    global hp, san
+    global q1, q2, q3
+    if r == 1:
+        print('아파트에 도착했다. 무엇을 할까?')
+        ans = int(input('1: 웅성이는 집문을 두드려본다 2: 반쯤 열려있는 듯한 문을 열어본다 3: 옥상으로 올라간다'))
+        if ans == 1:
+            print('갑자기 조용해지더니 긴장감이 맴돈다. 급하게 밖으로 뛰어나왔다')
+        elif ans == 2:
+            if rd.randint(1, 10) <= 8:
+                print('집에 사람은 없고 약간의 식량만 있었다. 체력 상승 1')
+                if hp < 3:
+                    hp += 1
+            else:
+                print('문에는 부비트랩이 설치되어 있었다.. 사망')
+                hp = 0
+        else:    
+            if rd.randint(1, 10) <= 8:
+                print('옥상은 잠겨있어 들어갈 수 없다.')
+            else:
+                print('로제와 브루노 마스가 옥상에 있었다. 아파트~ 아파트~ 히든 엔딩')
+                exit(0)
+    elif r == 2:
+        print('백화점에 도착했다. 무엇을 할까?')
+        ans = int(input('1:옷가게로 간다 2:식료품점으로 간다 3: 명품관에 간다'))
+        if ans == 1:
+            print('간만에 멀쩡한 옷을 마련했다. 정신력 상승 1')
+            if san < 3:
+                    san += 1
+        elif ans == 2:
+            if rd.randint(1, 3) > 2:
+                print("식료품 가판대 사이에서 보존식품을 찾았다")
+            else:
+                print("아무것도 찾지 못했다")
+        else:
+            print('경보가 울리더니 이내 문이 잠겼다. 안에 고립됬다.')
+            print('무엇을 해야할까?')
+            act = int(input('1.근처의 무거운 물건을 유리창으로 던진다 2.사람의 힘으로 할 영역이 아닌 것 같다. 포기한다'))
+            if act == 1:
+                if rd.randint(1, 8) < 3:
+                    print('유리창이 깨져 탈출할 수 있었다.')
+                elif rd.randint(1, 8) <= 3 and rd.randint(1, 8) > 5:
+                    print('유리창이 깨져 탈출할 수 있었다. 하지만 유리파편이 튀어 상처를 입었다. 체력 하락 1')
+                    hp -= 1
+                elif rd.randint(1, 8) <= 5 and rd.randint(1, 8) > 7:
+                    print('유리창에 던진 무거운 물체가 튕겨나더니 내 머리로 날라왔다. 사망')
+                    hp = 0
+                else:
+                    print('유리창을 부숴보려 별짓을 다했으나 끄덕없다. 이 안에 완전히 고립됬다. 사망')
+                    hp = 0    
+            else:
+                if rd.randint(1, 8) < 3:
+                    print('가방에 있던 식량으로 몇일을 버텼으나 결국 아사했다. 사망')
+                    hp = 0
+                elif rd.randint(1, 8) <= 3 and rd.randint(1, 8) > 5:
+                    print('시간이 지나니 자동으로 잠겼던 문이 열렸다')
+                elif rd.randint(1, 8) <= 5 and rd.randint(1, 8) > 7:
+                    print('꼼짝없이 갇힌줄 알았지만 다행히도 직원전용 뒷문이 있었다.')
+                else:
+                    print('경보를 듣고 가게 앞으로 다른 생존자가 찾아왔다. 생존자들은 총기로 무장한듯 보인다')
+                    print('무엇을 해야할까?')
+                    act = int(input('1.아무도 없는 척 숨어본다 2.살려달라고 소리친다'))
+                    if act == 1:
+                        print('안을 몇번 살펴보더니 이내 사라졌다. 도움을 요청해야하나 고민했지만 혼자 죽는 것보다 사람들이 더 무섭다. 사망')
+                        hp = 0
+                    else:
+                        if rd.randint(1, 2) == 1:
+                            print('그들은 주변에서 악명높은 약탈자였다. 살려달라 하염없이 빌었지만 소용없었다. 사망')
+                            hp = 0
+                        else:
+                            print('생존자들은 다행히도 좋은 사람들이였다. 경보가 들리길래 구하러 와줬다고 한다. 해피엔딩')
+                            exit(0)
+    else:
+        print("버려진 창고를 발견했다")
+        ans = int(input("1: 들어간다 2: 무시한다    "))
+        if ans == 1:
+            if rd.randint(1, 10) > 7:
+                print("총과 총알 3발을 찾았다. ")
+                q3 = 1
+            else:
+                print("빈 창고였다")
--- main.py
+++ main.py
@@ -10,11 +10,8 @@ import event as ev
 # 이벤트마다 선택지에 다른 결과가 출력됨
 
 
-
-
-# for문으로 6번 반복
 for i in range(1, 8):
-    if i == 0:
+    if i == 1:
         print("순천 2033")
         print("전쟁으로 인해 황폐해진 도시. 멀지 않은 도시에서 일어난 폭발이 핵폭탄이라는 말도있다.")
         print("그 상황에서 당신은 생존하기위한 도구를 찾아나선다.")
@@ -22,13 +19,16 @@ for i in range(1, 8):
     print(f"---{i}일차---")
     print(f"현재 건강상태 체력: {ev.hp} 정신력: {ev.san}")
     # i값은 날짜를 의미. 3일 이하를 전반기, 그 이후를 후반기로 보고 값은 나눔
-    # 현재 날짜가 3일 이하일 시 day1함수 작동
+    # 현재 날짜가 3일 이하일 시 event1함수 작동
     if i <= 3:
-        ev.day1(rd.randint(1, 3)) # randint(n, m) n에서 m사이의 정수 반환
-
-    # 현재 날짜가 4 이상일 시 day2 작동
+        ev.event1(rd.randint(1, 3)) # randint(n, m) n에서 m사이의 정수 반환
+    elif i >=4 and i <= 5:
+        ev.event2(rd.randint(1, 2))
+    else:
+        ev.event3()
+    # 현재 날짜가 4 이상일 시 event2 작동
     #else:
-    #   ev.day2(rd.randint(1, 3))
+    #   ev.event2(rd.randint(1, 3))
     if ev.hp == 0 or ev.san == 0:
         print("여기까지인듯 하다...")
         print("당신은 죽었습니다")
@@ -39,15 +39,30 @@ for i in range(1, 8):
     if i == 7:
         print("다시한번 폭탄이 떨어져습니다\n다행히 당신은 대피소 근처였고 서둘러 숨었으나 그곳엔 당신 뿐입니다\n이제 당신은 홀로 버텨야됩니다")
         ti.sleep(1)
-        if ev.q1 and ev.q2 and ev.q3:
-            print("당신은 물건들을 활용해 버티는데 성공했습니다\n이제 당신은 다시 밖으로 나갈 차례입니다\nHappy Ending")
-        elif ev.q1 and ev.q2:
-            print("식량을 조금씩 나눠먹으며 버틴지 20일차, 라디오에서 소리가 들립니다")
-            print("'생존하신 시민여러분은 밖으로 나와주시기 바랍니다. 전쟁은 끝났습니다.'")
-            print("당신은 고민에 빠졌습니다")
-            ans = int(input("1: 나간다 2: 가만히 있는다"))
+        if ev.q1:
+            if ev.q2 and ev.q3:
+                print("당신은 물건들을 활용해 버티는데 성공했습니다\n이제 당신은 다시 밖으로 나갈 차례입니다\nHappy Ending")
+            elif ev.q2:
+                print("식량을 조금씩 나눠먹으며 버틴지 20일차, 라디오에서 소리가 들립니다")
+                print("'생존하신 시민여러분은 밖으로 나와주시기 바랍니다. 전쟁은 끝났습니다.'")
+                print("당신은 고민에 빠졌습니다")
+                ans = int(input("1: 나간다 2: 가만히 있는다    "))
+                if ans == 1:
+                    if rd.randint(1, 10) > 5:
+                        print("당신은 국군과 조우했습니다. 다행히 전쟁은 끝났으나 군대에 징집되게 되었습니다.")
+                        print("Ending n.3 -Call of Duty")
+                    else:
+                        print("방송은 빨갱이들의 작전이였습니다. 당신은 그저 무력하게 끌려가는 수 밖에 없습니다.")
+                        print("Ending n.4 -Call of Aoji")
+                else:
+                    print("당신은 자꾸만 떠오르는 의구심에 밖으로 나가지 못했습니다.")
+            else:
+                print("당신은 벽이 무너졌는지 열리지 않는 문을 열지 못하고 벙커 안에서 라디오만 붙들다 쓰러졌습니다")
+                print("Bad Ending n.6 -radio")
         else:
-            print("슬프게도 당신은 생존에 도움될 물건이 부족했습니다\n홀로 벙커안에서 쓸쓸히 무너지게 되었습니다\nBad Ending")
-        exit(0)
+            print("슬프게도 당신은 생존에 도움될 물건이 부족했습니다\n홀로 벙커안에서 쓸쓸히 무너지게 되었습니다")
+            print("Bad Ending n.10 -Komm, süßer Tod")
+        
+
 
 exit(0)
